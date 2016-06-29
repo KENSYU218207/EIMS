b88ca4d73f5c7e281b1faa33bf9a05c450b45d37
井村 覚 <imura_s@mti.co.jp>
2016-06-29 16:18:33 +0900

    EmployeeDao競合解決

--- src/dao/EmployerDao.java
+++ src/dao/EmployerDao.java
@@ -169,72 +169,19 @@ public class EmployerDao {
 		}
 	}
 
-<<<<<<< HEAD
-	public static ArrayList<Employee> deleteSelectEmployees(String emptno) {
-=======
 	public static boolean deleteEmployees(String empno) {
->>>>>>> 6e350a709a95a04bb856e9463f9fe2090e4f0f8c
 
 		ArrayList<Employee> array = null;
 		Statement stmt = null;
 		ResultSet res = null;
 
 		try {
-<<<<<<< HEAD
-			String sql = "SELECT * "
-					+ "FROM employee LEFT OUTER JOIN department "
-					+ "ON employee.deptno = department.deptno "
-					+ "WHERE employee.deptno = " + "'" + emptno + "'" + " ";
 
-			stmt = con.createStatement();
-			res = stmt.executeQuery(sql);
-
-			while (res.next()) {
-				if (array == null) {
-					array = new ArrayList<Employee>();
-				}
-
-				Employee data = new Employee();
-				//DBの中身を取得して社員インスタンスにセットする
-				data.setEmpno(res.getInt("empno"));
-				data.setFname(res.getString("fname"));
-				data.setFkana(res.getString("fkana"));
-				data.setLname(res.getString("lname"));
-				data.setLkana(res.getString("lkana"));
-
-				array.add(data);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
-		return array;
-	}
-
-	public static ArrayList<Employee> deleteEmployees(String emptno) {
-
-		ArrayList<Employee> array = null;
-		Statement stmt = null;
-		ResultSet res = null;
-
-		try {
-			String sql = "SELECT * "
-					+ "FROM employee LEFT OUTER JOIN department "
-					+ "ON employee.deptno = department.deptno "
-					+ "WHERE lname = " + "'" + emptno + "'" + " "
-					+ "OR fname = " + "'" + emptno + "'" + " "
-					+ "OR lkana = " + "'" + emptno + "'" + " "
-					+ "OR fkana = " + "'" + emptno + "'" + " "
-					+ "OR department.deptname = " + "'" + emptno + "'" + " "
-					+ "OR employee.deptno = " + "'" + emptno + "'" + " ";
-=======
 			//Connectionの設定
 			Class.forName("com.mysql.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost/eimsdb", "eimsuser", "eimspass");
 
 			String sql = "DELETE FROM employee WHERE empno = " + empno + " ";
->>>>>>> 6e350a709a95a04bb856e9463f9fe2090e4f0f8c
 
 			stmt = con.createStatement();
 			res = stmt.executeQuery(sql);
