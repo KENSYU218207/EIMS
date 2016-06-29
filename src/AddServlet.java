89b8eaa690720882ebb937aedc3b3a72c71c799f
unknown <tsuda_h@PC5321.mti.co.jp>
2016-06-29 17:00:36 +0900

    おこられたけどいきます。

--- src/AddServlet.java
+++ src/AddServlet.java
@@ -26,7 +26,34 @@ public class AddServlet extends HttpServlet {
 
 //		String userid = request.getParameter("userid");
 
+		//追加画面で入力された情報格納
+		String empno = request.getParameter("addempno");
+		String lname = request.getParameter("addlname");
+		String fname = request.getParameter("addfname");
+		String lkana = request.getParameter("addlkana");
+		String fkana = request.getParameter("addfkana");
+		String password = request.getParameter("addpassword");
+		String gender = request.getParameter("addgender");
+		String deptno = request.getParameter("adddeptno");
+
+		if(empno==null||lname==null||fname==null||lkana==null||fkana==null||password==null){
+			empno="";
+			lname="";
+			fname="";
+			lkana="";
+			fkana="";
+			password="";
+		}
+
 		HttpSession session = request.getSession();
+		session.setAttribute("addempno", empno);
+		session.setAttribute("addlname", lname);
+		session.setAttribute("addfname", fname);
+		session.setAttribute("addlkana", lkana);
+		session.setAttribute("addfkana", fkana);
+		session.setAttribute("addpassword", password);
+		session.setAttribute("addgender", gender);
+		session.setAttribute("adddeptno", deptno);
 //		session.setAttribute("userid", userid);
 
 /*
@@ -58,15 +85,18 @@ public class AddServlet extends HttpServlet {
 //		out.println("人事部チェック"+dao.EmployerDao.checkJinji(userid));
 
 		out.println("<form action=\"AddConfirmServlet\" method=\"post\">");
-		 out.println("社員番号：<input type=\"text\" name=\"addempno\" size=\"20\"><br>");
-		 out.println("氏：<input type=\"text\" name=\"addlname\" size=\"20\"><br>");
-		 out.println("名：<input type=\"text\" name=\"addfname\" size=\"20\"><br>");
-		 out.println("氏（カナ）：<input type=\"text\" name=\"addlkana\" size=\"20\"><br>");
-		 out.println("名（カナ）：<input type=\"text\" name=\"addfkana\" size=\"20\"><br>");
-		 out.println("パスワード：<input type=\"password\" name=\"addpassword\" size=\"20\"><br>");
+		 out.println("社員番号：<input type=\"text\" name=\"addempno\" size=\"20\" value=\""+empno+"\"><br>");
+		 out.println("氏：<input type=\"text\" name=\"addlname\" size=\"20\" value=\""+lname+"\"><br>");
+		 out.println("名：<input type=\"text\" name=\"addfname\" size=\"20\" value=\""+fname+"\"><br>");
+		 out.println("氏（カナ）：<input type=\"text\" name=\"addlkana\" size=\"20\" value=\""+lkana+"\"><br>");
+		 out.println("名（カナ）：<input type=\"text\" name=\"addfkana\" size=\"20\" value=\""+fkana+"\"><br>");
+		 out.println("パスワード：<input type=\"password\" name=\"addpassword\" size=\"20\" value=\""+password+"\"><br>");
 		 out.println("性別：<input type=\"radio\" name=\"addgender\" value=\"1\">男性    <input type=\"radio\" name=\"addgender\" value=\"2\">女性<br>");
 		 out.println("所属コード：<select name=\"adddeptno\"><option value=\"none\">-</option><option value=\"100\">100：人事部</option><option value=\"200\">200：経理部</option><option value=\"300\">300：営業部</option><option value=\"400\">400：企画部</option><option value=\"500\">500：開発部</option><option value=\"600\">600：総務部</option></select><br>");
 		 out.println("<input type=\"submit\" value=\"追加する\">");
+		 out.println("</form>");
+	 	 out.println("<form action=\"SearchServlet\" method=\"post\">");
+		 out.println("<input type=\"submit\" value=\"キャンセル\">");
 		 out.println("</form>");
 
 		out.println("<br>");
