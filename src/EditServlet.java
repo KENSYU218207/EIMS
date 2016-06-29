9b57c9fc44286913eeea7fe3ed0c64758f885a9f
Satosh iSuzuki <suzuki_sa@mti.co.jp>
2016-06-29 15:39:56 +0900

    変更画面ほぼ完成

--- src/EditServlet.java
+++ src/EditServlet.java
@@ -26,6 +26,9 @@ public class EditServlet extends HttpServlet {
 		String lkana = request.getParameter("emp_lkana");
 		String fname = request.getParameter("emp_fname");
 		String fkana = request.getParameter("emp_fkana");
+		String dept = request.getParameter("emp_dept");
+		String gender = request.getParameter("emp_gender");
+		String password = request.getParameter("emp_pass");
 
 		//ログイン画面で入力された社員番号格納
 
@@ -66,9 +69,9 @@ public class EditServlet extends HttpServlet {
 		 out.println("名：<input type=\"text\" name=\"editfname\" size=\"20\" value=\"" + fname + "\"><br>");
 		 out.println("氏(カナ)：<input type=\"text\" name=\"editlkana\" size=\"20\" value=\"" + lkana + "\"><br>");
 		 out.println("名(カナ)：<input type=\"text\" name=\"editfkana\" size=\"20\" value=\"" + fkana + "\"><br>");
-		 out.println("パスワード：<input type=\"password\" name=\"editpassword\" size=\"20\"><br>");
+		 out.println("パスワード：<input type=\"password\" name=\"editpassword\" size=\"20\" value=\"" + password + "\"><br>");
 		 out.println("性別：<input type=\"radio\" name=\"editgender\" value=\"1\">男性    <input type=\"radio\" name=\"addgender\" value=\"2\">女性<br>");
-		 out.println("所属先：<select name=\"adddept\"><option value=\"none\">-</option><option value=\"100\">人事部</option><option value=\"200\">経理部</option><option value=\"300\">営業部</option><option value=\"400\">企画部</option><option value=\"500\">開発部</option><option value=\"600\">総務部</option></select><br>");
+		 out.println("所属先：<select name=\"editdept\"><option value=\"none\">-</option><option value=\"100\">人事部</option><option value=\"200\">経理部</option><option value=\"300\">営業部</option><option value=\"400\">企画部</option><option value=\"500\">開発部</option><option value=\"600\">総務部</option></select><br>");
 		 out.println("<input type=\"submit\" value=\"変更する\">");
 		 out.println("</form>");
 
